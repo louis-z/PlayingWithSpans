21d69bd36a21f5123279779ebf455766f44015d2
Louis Zanella <louis.zanella@gmail.com>
2020-02-21 19:29:48 -0500

    New .NET Core console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayingWithSpans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
