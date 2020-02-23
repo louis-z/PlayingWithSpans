3f1d99394a8f42ae2056ff2afb3e78576038be2a
Louis Zanella <louis.zanella@gmail.com>
2020-02-23 17:29:21 -0500

    Begin playing with Span<T>...

--- Program.cs
+++ Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace PlayingWithSpans
 {
-    class Program
+    /// <summary>
+    /// Getting familiar with Span<T>...
+    /// </summary>
+    /// <see href="https://github.com/dotnet/corefxlab/blob/master/docs/specs/span.md"/>
+    /// <see href="https://docs.microsoft.com/en-us/archive/msdn-magazine/2018/january/csharp-all-about-span-exploring-a-new-net-mainstay"/>
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            TestSpanWithAllMemoryTypes();
+        }
+
+        private static void TestSpanWithAllMemoryTypes()
+        {
+            // managed memory
+            var arrayMemory = new byte[100];
+            var arraySpan = new Span<byte>(arrayMemory);
+
+            // native memory
+            var nativeMemory = Marshal.AllocHGlobal(100);
+            Span<byte> nativeSpan;
+            unsafe
+            {
+                nativeSpan = new Span<byte>(nativeMemory.ToPointer(), 100);
+            }
+
+            SafeSum(nativeSpan);
+            Marshal.FreeHGlobal(nativeMemory);
+
+            // stack memory
+            Span<byte> stackSpan = stackalloc byte[100];
+            SafeSum(stackSpan);
+        }
+
+        // this method does not care what kind of memory it works on
+        private static ulong SafeSum(Span<byte> bytes)
+        {
+            ulong sum = 0;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sum += bytes[i];
+            }
+
+            return sum;
         }
     }
 }
